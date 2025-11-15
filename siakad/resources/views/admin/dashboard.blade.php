491623c1fe612c354dae39fa518f7cee1213cfcb
salambaee <salamrizki613@gmail.com>
2025-11-15 23:12:54 +0700

    salam: fix cdn, route

--- siakad/resources/views/admin/dashboard.blade.php
+++ siakad/resources/views/admin/dashboard.blade.php
@@ -4,9 +4,8 @@
 <h1 class="text-2xl font-bold mb-4">Dashboard Admin</h1>
 
 <div class="grid grid-cols-3 gap-4">
-    <div class="bg-white p-4 shadow rounded">Total Mahasiswa: {{ $countMahasiswa }}</div>
-    <div class="bg-white p-4 shadow rounded">Total Dosen: {{ $countDosen }}</div>
-    <div class="bg-white p-4 shadow rounded">Total Mata Kuliah: {{ $countMatkul }}</div>
+    <div class="bg-white p-4 shadow rounded">Total Mahasiswa:</div>
+    <div class="bg-white p-4 shadow rounded">Total Dosen:</div>
+    <div class="bg-white p-4 shadow rounded">Total Mata Kuliah:</div>
 </div>
 @endsection
-
--- siakad/resources/views/layouts/admin.blade.php
+++ siakad/resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ $title ?? 'Admin SIAKAD' }}</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100">
 
