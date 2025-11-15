d2db87cea987dedc492333a9c6325fbbd4f76fe4
mjvnaaa <dilan271594@gmail.com>
2025-11-16 01:10:48 +0700

    penambahan sedeers

--- siakad/database/seeders/MataKuliahSeeder.php
+++ siakad/database/seeders/MataKuliahSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MataKuliahSeeder extends Seeder
 {
@@ -12,6 +12,55 @@ class MataKuliahSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('mata_kuliah')->insert([
+            [
+                'kode_mk' => 'IF101',
+                'id_prodi' => 1,
+                'nama_mk' => 'Pemrograman Dasar',
+                'sks' => 3
+            ],
+            [
+                'kode_mk' => 'IF102',
+                'id_prodi' => 1,
+                'nama_mk' => 'Basis Data',
+                'sks' => 3
+            ],
+            [
+                'kode_mk' => 'IF103',
+                'id_prodi' => 1,
+                'nama_mk' => 'Jaringan Komputer',
+                'sks' => 3
+            ],
+            [
+                'kode_mk' => 'IF104',
+                'id_prodi' => 1,
+                'nama_mk' => 'Sistem Informasi',
+                'sks' => 3
+            ],
+            [
+                'kode_mk' => 'IF105',
+                'id_prodi' => 1,
+                'nama_mk' => 'Kecerdasan Buatan',
+                'sks' => 3
+            ],
+            [
+                'kode_mk' => 'IF106',
+                'id_prodi' => 1,
+                'nama_mk' => 'Elektronika Dasar',
+                'sks' => 2
+            ],
+            [
+                'kode_mk' => 'IF107',
+                'id_prodi' => 1,
+                'nama_mk' => 'UI/UX Design',
+                'sks' => 2
+            ],
+            [
+                'kode_mk' => 'IF108',
+                'id_prodi' => 1,
+                'nama_mk' => 'Cloud Computing',
+                'sks' => 3
+            ],
+        ]);
     }
 }
