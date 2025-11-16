a6151c938cdf1f303dc13899e4fa53a9ef501ec3
mjvnaaa <dilan271594@gmail.com>
2025-11-16 17:49:05 +0700

    uji coba

--- siakad/database/seeders/ProdiSeeder.php
+++ siakad/database/seeders/ProdiSeeder.php
@@ -15,15 +15,48 @@ class ProdiSeeder extends Seeder
         DB::table('prodi')->insert([
             [
                 'id_prodi' => 1,
-                'nama_prodi' => 'Teknologi Rekayasa Perangkat Lunak'
+                'nama_prodi' => 'D4 Teknologi Rekayasa Perangkat Lunak',
+                'jenjang' => 'D4',
             ],
             [
                 'id_prodi' => 2,
-                'nama_prodi' => 'Teknologi Rekayasa Komputer'
+                'nama_prodi' => 'D4 Bisnis Digital',
+                'jenjang' => 'D4',
             ],
             [
                 'id_prodi' => 3,
-                'nama_prodi' => 'Bisnis Digital'
+                'nama_prodi' => 'D4 Teknologi Rekayasa Komputer',
+                'jenjang' => 'D4',
+            ],
+            [
+                'id_prodi' => 4,
+                'nama_prodi' => 'D3 Teknik Sipil',
+                'jenjang' => 'D3',
+            ],
+            [
+                'id_prodi' => 5,
+                'nama_prodi' => 'D4 Teknologi Rekayasa Konstruksi Jalan dan Jembatan',
+                'jenjang' => 'D4',
+            ],
+            [
+                'id_prodi' => 8,
+                'nama_prodi' => 'D4 Teknologi Rekayasa Manufaktur',
+                'jenjang' => 'D4',
+            ],
+            [
+                'id_prodi' => 10,
+                'nama_prodi' => 'D4 Manajemen Bisnis Pariwisata',
+                'jenjang' => 'D4',
+            ],
+            [
+                'id_prodi' => 13,
+                'nama_prodi' => 'D4 Agribisnis',
+                'jenjang' => 'D4',
+            ],
+            [
+                'id_prodi' => 15,
+                'nama_prodi' => 'D4 Pengembangan Produk Agroindustri',
+                'jenjang' => 'D4',
             ],
         ]);
     }
